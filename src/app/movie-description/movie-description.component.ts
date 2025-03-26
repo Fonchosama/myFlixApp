1593040c62981381873fefe10956342ef80f0c39
GianBo <gianbo251@gmail.com>
2025-03-26 20:05:38 +0100

    Update movie-description.component.ts

--- src/app/movie-description/movie-description.component.ts
+++ src/app/movie-description/movie-description.component.ts
@@ -1,6 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+
+/**
+ * @description
+ * MovieDescriptionComponent is used to display detailed information about a movie in a dialog.
+ * It receives movie data as input and provides a way to close the dialog.
+ */
 @Component({
   selector: 'app-movie-description',
   standalone: false,
@@ -8,6 +14,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrl: './movie-description.component.scss'
 })
 export class MovieDescriptionComponent {
+
+    /**
+   * @constructor
+   * Creates an instance of MovieDescriptionComponent.
+   * @param {MatDialogRef<MovieDescriptionComponent>} dialogRef - A reference to the dialog, used to close it.
+   * @param {any} data - The data injected into the dialog, which contains the movie details.
+   */
   constructor(
     public dialogRef: MatDialogRef<MovieDescriptionComponent>,
     // pass the entire movie object to dialog as data
